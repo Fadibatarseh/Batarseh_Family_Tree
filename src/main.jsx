8550a9b523417a53e2e917fa29a904e49729d192
Fadibatarseh <MRBATARSEH@GMAIL.COM>
2025-12-11 20:43:41 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FamilyTreeApp from "./FamilyTreeApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FamilyTreeApp />
+  </React.StrictMode>
+);
