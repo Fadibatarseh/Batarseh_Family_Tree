11862234bf02058497e2a9b8351c173473737231
Fadibatarseh <MRBATARSEH@GMAIL.COM>
2025-12-13 13:21:07 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
 
+// REPLACE THESE WITH YOUR KEYS FROM SUPABASE DASHBOARD -> SETTINGS -> API
+const supabaseUrl = https://aitlgoljcxztolqbilki.supabase.co
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpdGxnb2xqY3h6dG9scWJpbGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2Mzc5ODMsImV4cCI6MjA4MTIxMzk4M30.DTAhRGQkmYRJSdkfQ_QlecJ6rRU21G1S82meu3IJJQU
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
