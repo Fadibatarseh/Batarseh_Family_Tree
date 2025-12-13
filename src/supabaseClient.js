a1b98edb670ad12b1eb2ffbf38285d1ba35b585c
Fadibatarseh <MRBATARSEH@GMAIL.COM>
2025-12-13 17:04:27 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// I added the single quotes ' ' around the URL and the Key below
+// 1. Go to Supabase Dashboard -> Project Settings (Gear icon) -> API
+// 2. Copy "Project URL" and paste it below
 const supabaseUrl = 'https://aitlgoljcxztolqbilki.supabase.co'
 
-// MAKE SURE THIS KEY IS ALSO WRAPPED IN QUOTES LIKE 'eyJ...'
-// (I am using the start of the key you showed in the error log)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpdGxnb2xqY3h6dG9scWJpbGtp...'
+// 3. Copy "anon" / "public" key and paste it below
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpdGxnb2xqY3h6dG9scWJpbGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2Mzc5ODMsImV4cCI6MjA4MTIxMzk4M30.DTAhRGQkmYRJSdkfQ_QlecJ6rRU21G1S82meu3IJJQU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
